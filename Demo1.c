af3a3b1bf9cd95edb02ae5322727470c1f3d6d65
Samrudhi2711 <samgadge27@gmail.com>
2025-08-31 14:45:31 +0530

    Initial commit

---
+++ Demo1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>//instruction 1
+
+/*
+    Multiline Commenting
+    My 1st C Program
+    Welcome to Sunbeam
+
+*/
+
+int main()//instruction 2
+{
+
+    printf("Hello Sunbeam...!");// instruction 3
+    printf("Hello Students...!");// instruction 4
+}
